87ca5ed0c3ff75e007395c2e67817eba5f77671e
Rodrigo Gonzalez <r@rodrigo-gonzalez.com>
2023-08-21 20:55:24 -0400

    bump: version 0.0.1 → 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## v0.1.0 (2023-08-21)
+
+### Feat
+
+- **main**: add entry point
+- **check-mkdocs**: initial commit with new check
--- src/check_mkdocs/__init__.py
+++ src/check_mkdocs/__init__.py
@@ -1,5 +1,5 @@
 from check_mkdocs.check_mkdocs import main
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 __all__ = ["main", "__version__"]
